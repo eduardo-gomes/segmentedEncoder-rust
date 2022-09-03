1eeccc1b5b263f2a56124058f376804bb102c034
Eduardo M Gomes <me@emg.dev>
2022-09-03 12:22:27 +0000

    Move gRPC service to submodule

--- example/src/server.rs
+++ example/src/server.rs
@@ -1,57 +1,62 @@
-use std::sync::{Arc, RwLock};
+use tonic::transport::Server;
 
-use futures_util::StreamExt; //stream.next()
-use tonic::{transport::Server, Request, Response, Status};
-
-use status::status_reporter_server::{StatusReporter, StatusReporterServer};
-use status::{Empty, ReportResult, StatusReport};
+use status::status_reporter_server::StatusReporterServer;
 
 pub mod status {
 	tonic::include_proto!("status");
 }
 
-pub struct StatusKeeper {
-	latest: Arc<RwLock<Option<StatusReport>>>,
-}
-impl StatusKeeper {
-	pub fn new() -> Self {
-		StatusKeeper {
-			latest: Arc::new(RwLock::new(None)),
+pub mod grpc {
+	use futures_util::StreamExt; //stream.next()
+	use std::sync::{Arc, RwLock};
+	use tonic::{Request, Response, Status};
+
+	use super::status::status_reporter_server::StatusReporter;
+	use super::status::{Empty, ReportResult, StatusReport};
+
+	pub struct StatusKeeper {
+		latest: Arc<RwLock<Option<StatusReport>>>,
+	}
+	impl StatusKeeper {
+		pub fn new() -> Self {
+			StatusKeeper {
+				latest: Arc::new(RwLock::new(None)),
+			}
 		}
 	}
-}
 
-#[tonic::async_trait]
-impl StatusReporter for StatusKeeper {
-	async fn report(
-		&self,
-		request: Request<tonic::Streaming<StatusReport>>,
-	) -> Result<Response<ReportResult>, Status> {
-		let mut stream = request.into_inner();
+	#[tonic::async_trait]
+	impl StatusReporter for StatusKeeper {
+		async fn report(
+			&self,
+			request: Request<tonic::Streaming<StatusReport>>,
+		) -> Result<Response<ReportResult>, Status> {
+			let mut stream = request.into_inner();
 
-		while let Some(report) = stream.next().await {
-			let report = report?;
-			println!("Received report: {:#?}", report);
-			let mut latest = self
+			while let Some(report) = stream.next().await {
+				let report = report?;
+				println!("Received report: {:#?}", report);
+				let mut latest = self
+					.latest
+					.write()
+					.map_err(|poison| Status::internal(poison.to_string()))?;
+				*latest = Some(report);
+			}
+			Ok(Response::new(ReportResult {}))
+		}
+		async fn get_latest_report(
+			&self,
+			_request: Request<Empty>,
+		) -> Result<Response<StatusReport>, Status> {
+			let report = self
 				.latest
-				.write()
-				.map_err(|poison| Status::internal(poison.to_string()))?;
-			*latest = Some(report);
-		}
-		Ok(Response::new(ReportResult {}))
-	}
-	async fn get_latest_report(
-		&self,
-		_request: Request<Empty>,
-	) -> Result<Response<StatusReport>, Status> {
-		let report = self
-			.latest
-			.read()
-			.map_err(|poison| Status::internal(poison.to_string()))?
-			.to_owned();
-		match report {
-			Some(report) => Ok(Response::new(report)),
-			None => Err(Status::unavailable("No report available")),
+				.read()
+				.map_err(|poison| Status::internal(poison.to_string()))?
+				.to_owned();
+			match report {
+				Some(report) => Ok(Response::new(report)),
+				None => Err(Status::unavailable("No report available")),
+			}
 		}
 	}
 }
@@ -60,7 +65,7 @@ impl StatusReporter for StatusKeeper {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let addr = "[::1]:8888".parse().unwrap();
 
-	let status_reporter_service = StatusKeeper::new();
+	let status_reporter_service = grpc::StatusKeeper::new();
 
 	let svc = StatusReporterServer::new(status_reporter_service);
 
