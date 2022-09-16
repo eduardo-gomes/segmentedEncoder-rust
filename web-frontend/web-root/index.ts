fbfde4478e81bcff54ad8736c48393a591b74468
Eduardo M Gomes <me@emg.dev>
2022-09-16 14:28:19 -0300

    Extract css to file, reformat code

--- web-frontend/web-root/index.ts
+++ web-frontend/web-root/index.ts
@@ -5,20 +5,21 @@ const tabs = document.getElementById("tabs");
 const container = document.getElementById("container");
 const div_list = new Map<string, HTMLDivElement>();
 
-function add_tab(element: HTMLDivElement, label: string){
-    div_list.set(label, element);
-    const button = document.createElement("button");
-    button.innerText = label;
-    tabs.appendChild(button);
-    element.classList.add("disabled");
-    container.appendChild(element);
+function add_tab(element: HTMLDivElement, label: string) {
+	div_list.set(label, element);
+	const button = document.createElement("button");
+	button.innerText = label;
+	tabs.appendChild(button);
+	element.classList.add("disabled");
+	container.appendChild(element);
 
-    let callback = () => {
-        div_list.forEach((div) => div.classList.add("disabled"));
-        element.classList.remove("disabled");
-    };
-    button.addEventListener("click", callback)
+	let callback = () => {
+		div_list.forEach((div) => div.classList.add("disabled"));
+		element.classList.remove("disabled");
+	};
+	button.addEventListener("click", callback)
 }
+
 add_tab(status_div, "Status");
 add_tab(jobs_div, "Jobs");
 status_updater(500);
--- web-frontend/web-root/status.ts
+++ web-frontend/web-root/status.ts
@@ -1,18 +1,19 @@
 const status_div = document.createElement("div");
 const out = document.createElement("pre");
+status_div.insertAdjacentHTML("afterbegin", "Auto refreshing /latest:");
 status_div.appendChild(out);
 
 async function refresh() {
-    let res = await fetch("/latest");
-    if (res.status >= 400)
-        throw new Error(`Refresh got status codee: ${res.status}`);
-    out.innerText = await res.text();
-    return "Request got: " + res.status;
+	let res = await fetch("/latest");
+	if (res.status >= 400)
+		throw new Error(`Refresh got status codee: ${res.status}`);
+	out.innerText = await res.text();
+	return "Request got: " + res.status;
 }
 
 function callback(timeout: number) {
-    refresh().then(console.log)
-    setTimeout(callback, timeout, timeout);
+	refresh().then(console.debug)
+	setTimeout(callback, timeout, timeout);
 }
 
 export {callback as status_updater};
