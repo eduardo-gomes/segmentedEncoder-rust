3c36a490def78a178991133cec02cef3f47d8f47
Eduardo M Gomes <me@emg.dev>
2022-09-16 14:17:31 -0300

    Output bundled js to OUT_DIR from build script, refactor build.rs

--- web-frontend/build.rs
+++ web-frontend/build.rs
@@ -1,14 +1,16 @@
+use std::env;
 use std::fs::canonicalize;
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+	let generated_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("bundled_js");
+
 	// println!("cargo:rerun-if-changed=web-root"); //pack_web already does this
 	let path = std::path::PathBuf::from("web-root");
 	let path = canonicalize(path)?;
-	let mut entry = path.clone();
-	entry.push("index.ts");
-	let mut out = path.clone();
-	out.push("out.js");
+	let entry = path.join("index.ts");
+	let out = generated_path.join("out.js");
 	println!("Entry: {entry:?}, out:{out:?}");
 	let result = Command::new("npx")
 		.args([
@@ -22,6 +24,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		.success();
 	assert!(result, "Failed to bundle script!");
 
-	web_packer::pack_web(&path)?;
+	web_packer::pack_web(&path, Some(&generated_path))?;
 	Ok(())
 }
