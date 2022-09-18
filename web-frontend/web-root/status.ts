66037b10e678a3b271de525e5a5270836d054b30
Eduardo M Gomes <me@emg.dev>
2022-09-17 23:14:14 -0300

    Change loop from setTimeout to setInterval and catch promise.

--- web-frontend/web-root/status.ts
+++ web-frontend/web-root/status.ts
@@ -6,15 +6,18 @@ status_div.appendChild(out);
 async function refresh() {
 	let res = await fetch("/latest");
 	if (res.status >= 400)
-		throw new Error(`Refresh got status codee: ${res.status}`);
+		throw new Error(`Refresh got status code: ${res.status}`);
 	out.innerText = await res.text();
 	return "Request got: " + res.status;
 }
 
-function callback(timeout: number) {
-	refresh().then(console.debug)
-	setTimeout(callback, timeout, timeout);
+function status_updater(timeout: number) {
+	function handler() {
+		refresh().then(console.debug).catch(console.error)
+	}
+
+	setInterval(handler, timeout);
 }
 
-export {callback as status_updater};
+export {status_updater};
 export default status_div;
