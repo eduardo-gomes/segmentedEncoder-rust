b6e0a7875693bb55ded1a1d01695985ebf6df6ee
Eduardo M Gomes <me@emg.dev>
2022-09-29 20:41:30 -0300

    Make Job store Source and Params

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -15,8 +15,15 @@ pub fn make_service() -> Router {
 mod storage;
 
 mod jobs {
+	#[derive(Clone, Debug)]
+	#[cfg_attr(test, derive(PartialEq))]
 	pub struct JobParams {
 		pub(crate) video_encoder: String,
+	}
+	#[derive(Clone, Debug)]
+	#[cfg_attr(test, derive(PartialEq))]
+	pub(crate) enum Source {
+		Local(),
 	}
 
 	impl JobParams {
@@ -28,7 +35,32 @@ mod jobs {
 		}
 	}
 	#[derive(Debug)]
-	pub struct Job();
+	#[cfg_attr(test, derive(PartialEq))]
+	pub(crate) struct Job {
+		pub source: Source,
+		pub parameters: JobParams,
+	}
+
+	impl Job {
+		pub(crate) fn new(source: Source, parameters: JobParams) -> Self {
+			Job { source, parameters }
+		}
+	}
+
+	#[cfg(test)]
+	mod test {
+		use crate::jobs::{Job, JobParams, Source};
+
+		#[test]
+		fn job_takes_source_and_parameters() {
+			let source = Source::Local();
+			let parameters = JobParams::sample_params();
+			let job = Job::new(source.clone(), parameters.clone());
+
+			assert_eq!(job.source, source);
+			assert_eq!(job.parameters, parameters);
+		}
+	}
 }
 
 #[allow(dead_code)]
