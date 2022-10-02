94a70d28a8416a8c1f64188ea20da89b6836540c
Eduardo M Gomes <me@emg.dev>
2022-10-02 09:45:57 -0300

    Replace FuturesAsyncReadCompatExt with StreamReader

--- server/src/storage.rs
+++ server/src/storage.rs
@@ -87,22 +87,19 @@ pub mod stream {
 	use hyper::Body;
 	use tokio::fs::File;
 	use tokio::io::AsyncRead;
-	use tokio_util::io::ReaderStream;
+	use tokio_util::io::{ReaderStream, StreamReader};
 
 	pub(crate) async fn body_to_file(body: Body, file: &mut File) -> std::io::Result<u64> {
-		use futures::stream::TryStreamExt;
 		use futures::StreamExt;
-		let stream = body
-			.map(|res| {
-				res.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))
-			})
-			.into_async_read();
-		let mut stream = tokio_util::compat::FuturesAsyncReadCompatExt::compat(stream);
+		let body = body.map(|res| {
+			res.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e.to_string()))
+		});
+		let mut stream = StreamReader::new(body);
 		tokio::io::copy(&mut stream, file).await
 	}
 
 	pub(crate) fn read_to_stream<T: AsyncRead>(read: T) -> ReaderStream<T> {
-		tokio_util::io::ReaderStream::new(read)
+		ReaderStream::new(read)
 	}
 
 	#[cfg(test)]
