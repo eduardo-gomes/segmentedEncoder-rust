ad6ef8655725e3ebb3af42cc1a759278b0800acb
Eduardo M Gomes <me@emg.dev>
2022-10-20 00:12:46 -0300

    Create function to start server for integration tests

--- server/tests/grpc.rs
+++ server/tests/grpc.rs
@@ -1,3 +1,7 @@
+use std::error::Error;
+use std::future::Future;
+
+use hyper::Uri;
 use tonic::transport::Endpoint;
 use tonic::Code;
 
@@ -5,24 +9,35 @@ use grpc_proto::proto::segmented_encoder_client::SegmentedEncoderClient;
 use grpc_proto::proto::Empty;
 use server::make_multiplexed_service;
 
-#[tokio::test]
-async fn grpc_service_is_available_from_main_test_server() -> Result<(), Box<dyn std::error::Error>>
-{
-	let server = make_multiplexed_service();
-	let (url, handle, tx) = {
-		let addr = "[::1]:0".parse()?;
-		let server = hyper::Server::bind(&addr).serve(server);
-		let addr = server.local_addr();
-		let (tx, rx) = tokio::sync::oneshot::channel::<()>();
-		let graceful = server.with_graceful_shutdown(async {
-			rx.await.ok();
-		});
-		let server_handle = tokio::spawn(graceful);
-		let port = addr.port();
-		let url = format!("http://[::1]:{port}");
-		(url, server_handle, tx)
+async fn start_server(
+) -> Result<(impl Future<Output = Result<(), Box<dyn Error>>>, Uri), Box<dyn Error>> {
+	let service = make_multiplexed_service();
+	let addr = "[::1]:0".parse().unwrap();
+	let server = hyper::Server::bind(&addr).serve(service);
+	let addr = server.local_addr();
+	let (tx, rx) = tokio::sync::oneshot::channel::<()>();
+	let graceful = server.with_graceful_shutdown(async {
+		rx.await.ok();
+	});
+	let server_handle = tokio::spawn(graceful);
+	let port = addr.port();
+	let close = async move {
+		tx.send(()).map_err(|_| "the receiver dropped")?;
+		server_handle.await??;
+		Ok(())
 	};
-	let endpoint: Endpoint = url.parse()?;
+	let url = Uri::builder()
+		.scheme("http")
+		.authority(format! {"[::1]:{port}"})
+		.path_and_query("/")
+		.build()?;
+	Ok((close, url))
+}
+
+#[tokio::test]
+async fn grpc_service_is_available_from_main_test_server() -> Result<(), Box<dyn Error>> {
+	let (close, url) = start_server().await?;
+	let endpoint: Endpoint = url.into();
 	let mut client = SegmentedEncoderClient::connect(endpoint).await?;
 	let response = client
 		.get_worker_registration(Empty {})
@@ -33,6 +48,5 @@ async fn grpc_service_is_available_from_main_test_server() -> Result<(), Box<dyn
 		Code::Unauthenticated,
 		"The grpc service would return Unauthenticated"
 	);
-	tx.send(()).map_err(|_| "the receiver dropped")?;
-	Ok(handle.await??)
+	close.await
 }
