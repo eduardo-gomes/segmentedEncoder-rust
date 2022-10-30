c79d8333363ed683ae5e4e4b5bdbd01d0d96ef20
Eduardo M Gomes <me@emg.dev>
2022-10-30 16:31:36 -0300

    Refactor Segment allocation to JobSegmenter.
    Uses RAII to track Segment allocation

--- server/src/job_manager/segmenter.rs
+++ server/src/job_manager/segmenter.rs
@@ -1,5 +1,6 @@
 //! Module that manages tasks from jobs, should handle if task fails, and when job is complete.
 //! Also handle status tracking
+use std::sync::atomic::{AtomicU8, Ordering};
 use std::sync::Arc;
 
 use tokio::sync::{OnceCell, RwLock};
@@ -28,11 +29,126 @@ impl Segment {
 struct JobSegmenter {
 	job: Arc<Job>,
 	job_id: Uuid,
-	segment: OnceCell<(Segment, OnceCell<()>)>,
+	segments: OnceCell<SegmentEntry>,
+}
+
+type SegmentData = Arc<(Segment, AtomicU8)>;
+
+///Hold segment data
+struct SegmentEntry(SegmentData);
+
+///RAII wrapper for Segment allocation
+struct SegmentAllocation(SegmentData);
+
+enum AllocationState {
+	Queue = 0,
+	Allocated = 1,
+	Completed = 2,
+}
+
+impl SegmentAllocation {
+	fn as_segment(&self) -> &Segment {
+		&self.0 .0
+	}
+	fn set_completed(&self) -> usize {
+		let state = &self.0 .1;
+		state.store(AllocationState::Completed as u8, Ordering::Release);
+		0
+	}
+}
+
+impl Drop for SegmentAllocation {
+	fn drop(&mut self) {
+		let _ = self.0 .1.compare_exchange(
+			AllocationState::Allocated as u8,
+			AllocationState::Queue as u8,
+			Ordering::SeqCst,
+			Ordering::Acquire,
+		);
+	}
+}
+
+impl SegmentEntry {
+	fn allocate(&self) -> Option<SegmentAllocation> {
+		let bool = &self.0 .1;
+		let allocate = bool.compare_exchange(
+			AllocationState::Queue as u8,
+			AllocationState::Allocated as u8,
+			Ordering::SeqCst,
+			Ordering::Acquire,
+		);
+		allocate.map(|_| SegmentAllocation(self.0.clone())).ok()
+	}
+}
+
+impl JobSegmenter {
+	fn new(job: Arc<Job>, job_id: Uuid) -> Self {
+		Self {
+			job,
+			job_id,
+			segments: OnceCell::new(),
+		}
+	}
+	fn get_available(&self) -> Option<SegmentAllocation> {
+		self.segments
+			.get()
+			.or_else(|| self.next_segment())
+			.and_then(|segment| segment.allocate())
+	}
+}
+
+#[cfg(test)]
+mod job_segmenter_test {
+	use std::sync::Arc;
+
+	use uuid::Uuid;
+
+	use crate::job_manager::segmenter::JobSegmenter;
+	use crate::jobs::{Job, JobParams, Source};
+	use crate::storage::FileRef;
+
+	#[test]
+	fn job_segmenter_get_available_return_some_for_do_not_segment() {
+		let source = Source::File(FileRef::fake());
+		let parameters = JobParams::sample_params();
+		let job_uuid = Uuid::new_v4();
+		let job = Arc::new(Job::new(source, parameters));
+		let segmenter = JobSegmenter::new(job, job_uuid);
+
+		let available = segmenter.get_available();
+		assert!(available.is_some())
+	}
+
+	#[test]
+	fn job_segmenter_get_available_twice_return_none() {
+		let source = Source::File(FileRef::fake());
+		let parameters = JobParams::sample_params();
+		let job_uuid = Uuid::new_v4();
+		let job = Arc::new(Job::new(source, parameters));
+		let segmenter = JobSegmenter::new(job, job_uuid);
+
+		let _available = segmenter.get_available();
+		let available = segmenter.get_available();
+		assert!(available.is_none())
+	}
+
+	#[test]
+	fn job_segmenter_after_drop_can_allocate_again() {
+		let source = Source::File(FileRef::fake());
+		let parameters = JobParams::sample_params();
+		let job_uuid = Uuid::new_v4();
+		let job = Arc::new(Job::new(source, parameters));
+		let segmenter = JobSegmenter::new(job, job_uuid);
+
+		let available = segmenter.get_available();
+		drop(available);
+		let available = segmenter.get_available();
+		assert!(available.is_some())
+	}
 }
 
 pub(super) struct TaskScheduler {
-	allocated: RwLock<Option<Task>>,
+	allocated: RwLock<Option<(SegmentAllocation, Uuid)>>,
 	segmenter: JobSegmenter,
 }
 
@@ -44,25 +160,16 @@ impl TaskScheduler {
 	///The returned task will be marked as running.
 	pub(super) async fn allocate(&self) -> Option<Task> {
 		let mut allocated = self.allocated.write().await;
-		let finished = self
-			.segmenter
-			.segment
-			.get()
-			.and_then(|(_, finished)| finished.get())
-			.and(Some(true))
-			.unwrap_or_default();
-		if allocated.is_some() || finished {
-			return None;
-		}
 		let task = self
 			.segmenter
-			.segment
-			.get()
-			.map(|(segment, _)| segment)
-			.or_else(|| self.segmenter.next_segment())
-			.cloned()
-			.map(|segment| segment.into_task(&self.segmenter.job_id, &Uuid::new_v4()));
-		*allocated = task.clone();
+			.get_available()
+			.and_then(|segment| {
+				*allocated = Some((segment, Uuid::new_v4()));
+				allocated
+					.as_ref()
+					.map(|(seg, id)| (seg.as_segment().clone(), id))
+			})
+			.map(|(segment, id)| segment.into_task(&self.segmenter.job_id, id));
 		task
 	}
 
@@ -71,15 +178,20 @@ impl TaskScheduler {
 			.read()
 			.await
 			.as_ref()
-			.filter(|task| id == &task.id)
-			.cloned()
+			.filter(|(_, task_id)| task_id == id)
+			.map(|(segment, _)| {
+				segment
+					.as_segment()
+					.clone()
+					.into_task(&self.segmenter.job_id, &id)
+			})
 	}
 
 	pub(crate) async fn cancel_task(&self, id: &Uuid) -> Result<(), ()> {
 		let mut allocated = self.allocated.write().await;
 		let should_remove = allocated
 			.as_ref()
-			.and_then(|task| (&task.id == id).then_some(()));
+			.and_then(|(_, task_id)| (task_id == id).then_some(()));
 		if should_remove.is_some() {
 			*allocated = None;
 		}
@@ -88,19 +200,12 @@ impl TaskScheduler {
 
 	pub(crate) async fn set_task_as_completed(&self, task_id: &Uuid) -> Option<usize> {
 		let mut lock = self.allocated.write().await;
-		let take_is_match = |option: &mut Option<Task>| {
-			if option.as_ref().filter(|task| &task.id == task_id).is_some() {
-				option.take();
-				self.segmenter
-					.segment
-					.get()
-					.and_then(|(_, finished)| finished.set(()).ok());
-				Some(0)
-			} else {
-				None
-			}
-		};
-		take_is_match(&mut lock)
+		let should_remove = lock
+			.as_ref()
+			.and_then(|(_, id)| (id == task_id).then_some(()));
+		should_remove
+			.and_then(|()| lock.take())
+			.map(|(allocated, _)| allocated.set_completed())
 	}
 }
 
@@ -108,11 +213,7 @@ impl Job {
 	pub(super) fn make_segmenter(self: Arc<Self>, uuid: Uuid) -> TaskScheduler {
 		TaskScheduler {
 			allocated: RwLock::new(None), //While we only have one task, and don't restart
-			segmenter: JobSegmenter {
-				job: self,
-				job_id: uuid,
-				segment: OnceCell::new(),
-			},
+			segmenter: JobSegmenter::new(self, uuid),
 		}
 	}
 }
@@ -121,13 +222,16 @@ impl JobSegmenter {
 	///Internal function to segment jobs.
 	///
 	///This may differ for different kinds of segmentation.
-	fn next_segment(&self) -> Option<&Segment> {
+	fn next_segment(&self) -> Option<&SegmentEntry> {
 		let segment = Segment {
 			input_path: format!("/api/jobs/{}/source", self.job_id),
 			parameters: self.job.parameters.clone(),
 		};
-		let res = self.segment.set((segment, OnceCell::new()));
-		res.ok().and(self.segment.get().map(|(segment, _)| segment))
+		let segment = Arc::new((segment, AtomicU8::new(AllocationState::Queue as u8)));
+		self.segments
+			.set(SegmentEntry(segment))
+			.ok()
+			.and_then(|()| self.segments.get())
 	}
 }
 
