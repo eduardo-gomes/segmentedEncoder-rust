87e933d23e47252310b087c6caa8a5969ed0b8fd
Eduardo M Gomes <me@emg.dev>
2022-12-31 10:06:36 -0300

    Move functions to upload job to task.ts.
    Move functions interfacing api and related from jobs.tsx to task.ts.
    Adapt create_task return using promise instead of callback.
    Replace callback on file input with ref.

--- web-frontend/src/jobs.tsx
+++ web-frontend/src/jobs.tsx
@@ -1,66 +1,6 @@
-import { get_api_path } from "./lib/api";
 import { createSignal, Setter, Show } from "solid-js";
-
-function create_task(task: Task, setStatus: (status: string) => void) {
-	function send_fulfilled(res: Response) {
-		console.debug("Created task response:", res);
-		if (res.ok)
-			res.text().then(function (text) {
-				const url = get_api_path();
-				url.pathname += `/jobs/${text}/source`;
-				console.info("Source available at:", url.href);
-				setStatus("Created job " + text);
-			});
-		else {
-			console.warn("Request was not successful:", res);
-			setStatus("Created job bad response");
-		}
-	}
-
-	function send_rejected(e: unknown) {
-		console.error("Create task error:", e);
-		setStatus("Upload job request failed");
-	}
-
-	console.debug("Task to create:", task);
-
-	setStatus("Uploading job!");
-
-	send_task(task)
-		.then(send_fulfilled)
-		.catch(send_rejected);
-}
-
-type Task = {
-	video_encoder: string,
-	video_args: string,
-	audio_encoder: string,
-	audio_args: string,
-	file: File
-};
-
-function visible_ascii(str: string) {
-	//Only ascii will be allowed for now.
-	//For previous encoding: https://github.com/eduardo-gomes/segmentedEncoder-rust/blob/b82d0ea872d5784cedbac3003db6df6e09ccbf37/web-frontend/web-root/jobs.ts#L77
-	const match = str.match(/[^\x20-\x7E\t]|%/g);
-	if (match) throw Error("Found character not in visible ascii: " + match.join());
-	return str;
-}
-
-async function send_task(task: Task) {
-	const headers = {
-		video_encoder: visible_ascii(task.video_encoder),
-		video_args: visible_ascii(task.video_args),
-		audio_encoder: visible_ascii(task.audio_encoder),
-		audio_args: visible_ascii(task.audio_args),
-	};
-	console.debug("Encoded header:", headers);
-	return await fetch(get_api_path() + "/jobs", {
-		method: "POST",
-		headers: headers,
-		body: task.file
-	})
-}
+import type { Task } from "./lib/task";
+import { create_task } from "./lib/task";
 
 function JobsTab() {
 	const [videoCodec, setVideoCodec] = createSignal("libsvtav1");
@@ -68,15 +8,10 @@ function JobsTab() {
 	const [audioCodec, setAudioCodec] = createSignal("libopus");
 	const [audioArgs, setAudioArgs] = createSignal("-b:a 96k");
 
-	let file_list: FileList | null;
-
-	function update_files(event: Event) {
-		const file_input = event.target as HTMLInputElement;
-		file_list = file_input.files;
-	}
+	let file_input: HTMLInputElement | undefined;
 
 	function get_task() {
-		const files = file_list;
+		const files = file_input?.files;
 		if (files == null || files.length < 1) throw new Error("No file selected");
 		const task: Task = {
 			video_encoder: videoCodec(),
@@ -92,13 +27,24 @@ function JobsTab() {
 		return (e: Event & { currentTarget: HTMLInputElement }) => fn(e.currentTarget.value);
 	}
 
+	function onCreate() {
+		setStatus("Uploading job!");
+		create_task(get_task()).then((res) => {
+				if (!res.isErr)
+					setStatus("Created job " + res.job);
+				else
+					setStatus(res.text);
+			}
+		)
+	}
+
 	const [status, setStatus] = createSignal("");
 	return (<>
 			Add job:
 			<div id="job-div">
 				<label>
 					<span>Input file:</span>
-					<input type="file" accept="video/*" onChange={update_files}/>
+					<input ref={ref => file_input = ref} type="file" accept="video/*"/>
 				</label>
 				<label>
 					<span>video encoder:</span>
@@ -113,7 +59,7 @@ function JobsTab() {
 					<span>audio args:</span>
 					<input type="text" value={audioArgs()} onChange={textChange(setAudioArgs)}/>
 				</label>
-				<input type="button" value="Add job" onClick={() => create_task(get_task(), setStatus)}/>
+				<input type="button" value="Add job" onClick={onCreate}/>
 				<Show when={status()}>
 					<label>
 						<span>Status:</span>
---
+++ web-frontend/src/lib/task.ts
@@ -0,0 +1,62 @@
+import { get_api_path } from "./api";
+
+type Successful = { job: string, isErr: false };
+type Rejected = { text: string, isErr: true };
+
+export function create_task(task: Task): Promise<Rejected | Successful> {
+	async function send_fulfilled(res: Response): Promise<Rejected | Successful> {
+		console.debug("Created task response:", res);
+		if (res.ok) {
+			const text = await res.text();
+			const url = get_api_path();
+			url.pathname += `/jobs/${text}/source`;
+			console.info("Source available at:", url.href);
+			return {job: text, isErr: false};
+		} else {
+			console.warn("Request was not successful:", res);
+			return {text: "Created job bad response", isErr: true};
+		}
+	}
+
+	function send_rejected(e: unknown): Rejected {
+		console.error("Create task error:", e);
+		return {text: "Upload job request failed", isErr: true};
+	}
+
+	console.debug("Task to create:", task);
+
+	return send_task(task)
+		.then(send_fulfilled)
+		.catch(send_rejected);
+}
+
+export type Task = {
+	video_encoder: string,
+	video_args: string,
+	audio_encoder: string,
+	audio_args: string,
+	file: File
+};
+
+function visible_ascii(str: string) {
+	//Only ascii will be allowed for now.
+	//For previous encoding: https://github.com/eduardo-gomes/segmentedEncoder-rust/blob/b82d0ea872d5784cedbac3003db6df6e09ccbf37/web-frontend/web-root/jobs.ts#L77
+	const match = str.match(/[^\x20-\x7E\t]|%/g);
+	if (match) throw Error("Found character not in visible ascii: " + match.join());
+	return str;
+}
+
+async function send_task(task: Task) {
+	const headers = {
+		video_encoder: visible_ascii(task.video_encoder),
+		video_args: visible_ascii(task.video_args),
+		audio_encoder: visible_ascii(task.audio_encoder),
+		audio_args: visible_ascii(task.audio_args),
+	};
+	console.debug("Encoded header:", headers);
+	return await fetch(get_api_path() + "/jobs", {
+		method: "POST",
+		headers: headers,
+		body: task.file
+	})
+}
