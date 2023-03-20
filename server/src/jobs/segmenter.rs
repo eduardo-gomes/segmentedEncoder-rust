e1261d58465204fc388709f60409f3070a070186
Eduardo M Gomes <me@emg.dev>
2023-03-20 19:16:05 -0300

    Make segmenter return struct with array of TaskInfo

--- server/src/jobs/segmenter.rs
+++ server/src/jobs/segmenter.rs
@@ -14,14 +14,21 @@ impl Segmenter {
 	/// Will generate TaskInfos and SubJobs.
 	///
 	/// TaskInfo can be turned into a [Task](super::Task), and SubJob into a [Job]
-	fn segment(job: &Job) -> TaskInfo {
+	fn segment(job: &Job) -> JobSegments {
 		match job.segmenter {
-			Segmenter::DoNotSegment => TaskInfo {
-				input: job.source.clone(),
-				parameters: job.parameters.clone(),
-			},
+			Segmenter::DoNotSegment => {
+				let task = TaskInfo {
+					input: job.source.clone(),
+					parameters: job.parameters.clone(),
+				};
+				JobSegments { tasks: vec![task] }
+			}
 		}
 	}
+}
+
+struct JobSegments {
+	tasks: Vec<TaskInfo>,
 }
 
 struct TaskInfo {
@@ -34,10 +41,19 @@ mod test {
 	use crate::jobs::{Job, Segmenter};
 
 	#[test]
+	fn don_not_segment_generate_response_with_an_array_with_a_single_task_info() {
+		let job = Job::fake();
+		let segmented = Segmenter::segment(&job);
+		let array: Vec<_> = segmented.tasks;
+		assert_eq!(array.len(), 1);
+	}
+
+	#[test]
 	fn don_not_segment_generate_task_info_with_same_input_and_parameters() {
 		let job = Job::fake();
 		let segmented = Segmenter::segment(&job);
-		assert_eq!(segmented.parameters, job.parameters);
-		assert_eq!(segmented.input, job.source);
+		let task = segmented.tasks.first().expect("Should generate some");
+		assert_eq!(task.parameters, job.parameters);
+		assert_eq!(task.input, job.source);
 	}
 }
