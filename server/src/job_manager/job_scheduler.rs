e543942a9fcefb4cb2c8a9a89041a2c481f680db
Eduardo M Gomes <me@emg.dev>
2023-03-21 09:47:54 -0300

    Make job_scheduler allocate generating dummy TaskInfo

--- server/src/job_manager/job_scheduler.rs
+++ server/src/job_manager/job_scheduler.rs
@@ -10,33 +10,56 @@
 //! - **Post-execution**: Useful to merge all the artifacts into a single file
 //!
 
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
 use uuid::Uuid;
 
-use crate::jobs::{Job, Task};
+use crate::jobs::segmenter::TaskInfo;
+use crate::jobs::{Job, Source};
+use crate::storage::FileRef;
 
 pub(crate) struct JobScheduler {
 	job: Arc<Job>,
 	uuid: Uuid,
+	allocated: AtomicBool,
+}
+
+pub struct AllocatedTask {
+	task: TaskInfo,
 }
 
 impl JobScheduler {
 	pub(super) fn new(job: Arc<Job>, uuid: Uuid) -> Self {
-		Self { job, uuid }
+		Self {
+			job,
+			uuid,
+			allocated: false.into(),
+		}
 	}
 	/// Allocate tasks from the job
 	///
 	/// This function will not wait for tasks to be available.
 	///
 	/// The returned object contains all info the client needs to start processing
-	pub(super) async fn allocate(self: &Arc<Self>) -> Option<Task> {
-		None
+	pub(super) async fn allocate(&self) -> Option<AllocatedTask> {
+		let old = self.allocated.swap(true, Ordering::AcqRel);
+		if !old {
+			let task_info = TaskInfo {
+				input: Source::File(FileRef::from(Uuid::nil())),
+				parameters: self.job.parameters.clone(),
+			};
+			Some(AllocatedTask { task: task_info })
+		} else {
+			None
+		}
 	}
 }
 
 #[cfg(test)]
 mod test {
+	use std::sync::Arc;
+
 	use uuid::Uuid;
 
 	use crate::job_manager::job_scheduler::JobScheduler;
@@ -49,5 +72,35 @@ mod test {
 		let scheduler = JobScheduler::new(job, uuid);
 
 		assert_eq!(scheduler.uuid, uuid);
+	}
+
+	#[tokio::test]
+	async fn do_not_segment_job_allocate_return_some() {
+		let job = Job::fake().into();
+		let uuid = Uuid::new_v4();
+		let scheduler = JobScheduler::new(job, uuid);
+		let allocated = scheduler.allocate().await;
+
+		assert!(allocated.is_some());
+	}
+
+	#[tokio::test]
+	async fn do_not_segment_job_allocate_return_only_once() {
+		let job = Job::fake().into();
+		let uuid = Uuid::new_v4();
+		let scheduler = JobScheduler::new(job, uuid);
+		let _allocated = scheduler.allocate().await;
+		let allocated = scheduler.allocate().await;
+
+		assert!(allocated.is_none());
+	}
+
+	#[tokio::test]
+	async fn do_not_segment_job_allocatd_has_same_job_parameters() {
+		let job: Arc<_> = Job::fake().into();
+		let uuid = Uuid::new_v4();
+		let scheduler = JobScheduler::new(job.clone(), uuid);
+		let allocated = scheduler.allocate().await.expect("Should be available");
+		assert_eq!(allocated.task.parameters, job.parameters);
 	}
 }
