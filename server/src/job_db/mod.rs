21ebf149bc28b855dd69c3c4fb561f50e702daf5
Eduardo M Gomes <me@emg.dev>
2024-02-19 16:57:48 -0300

    Add and get task from local db

--- server/src/job_db/mod.rs
+++ server/src/job_db/mod.rs
@@ -22,11 +22,14 @@ use uuid::Uuid;
 trait JobDb<JOB, TASK> {
 	async fn get_job(&self, id: &Uuid) -> Result<Option<JOB>, std::io::Error>;
 	async fn create_job(&self, job: JOB) -> Result<Uuid, std::io::Error>;
+	/// Append task to job and return the task index
+	async fn append_task(&self, job_id: &Uuid, task: TASK) -> Result<usize, std::io::Error>;
+	async fn get_task(&self, job_id: &Uuid, task_idx: usize) -> Result<TASK, std::io::Error>;
 }
 
 mod local {
 	use std::collections::HashMap;
-	use std::io::Error;
+	use std::io::{Error, ErrorKind};
 	use std::sync::{Mutex, MutexGuard};
 
 	use async_trait::async_trait;
@@ -35,12 +38,12 @@ mod local {
 	use crate::job_db::JobDb;
 
 	#[derive(Default)]
-	pub struct LocalJobDb<JOB: Sync + Send + Clone, TASK: Sync + Send> {
-		jobs: Mutex<HashMap<Uuid, (JOB, HashMap<Uuid, TASK>)>>,
+	pub struct LocalJobDb<JOB: Sync + Send + Clone, TASK: Sync + Send + Clone> {
+		jobs: Mutex<HashMap<Uuid, (JOB, Vec<TASK>)>>,
 	}
 
-	impl<JOB: Sync + Send + Clone, TASK: Sync + Send> LocalJobDb<JOB, TASK> {
-		fn lock(&self) -> MutexGuard<'_, HashMap<Uuid, (JOB, HashMap<Uuid, TASK>)>> {
+	impl<JOB: Sync + Send + Clone, TASK: Sync + Send + Clone> LocalJobDb<JOB, TASK> {
+		fn lock(&self) -> MutexGuard<'_, HashMap<Uuid, (JOB, Vec<TASK>)>> {
 			self.jobs
 				.lock()
 				.unwrap_or_else(|poison| poison.into_inner())
@@ -48,7 +51,9 @@ mod local {
 	}
 
 	#[async_trait]
-	impl<JOB: Sync + Send + Clone, TASK: Sync + Send> JobDb<JOB, TASK> for LocalJobDb<JOB, TASK> {
+	impl<JOB: Sync + Send + Clone, TASK: Sync + Send + Clone> JobDb<JOB, TASK>
+		for LocalJobDb<JOB, TASK>
+	{
 		async fn get_job(&self, id: &Uuid) -> Result<Option<JOB>, Error> {
 			let job = self.lock().get(id).map(|(job, _)| job).cloned();
 			Ok(job)
@@ -59,10 +64,32 @@ mod local {
 			self.lock().insert(key, (job, Default::default()));
 			Ok(key)
 		}
+
+		async fn append_task(&self, job_id: &Uuid, task: TASK) -> Result<usize, Error> {
+			let mut guard = self.lock();
+			let job = match guard.get_mut(job_id).map(|(_, tasks)| tasks) {
+				None => return Err(Error::new(ErrorKind::NotFound, "Job not found")),
+				Some(tasks) => tasks,
+			};
+			let idx = job.len();
+			job.push(task);
+			Ok(idx)
+		}
+
+		async fn get_task(&self, job_id: &Uuid, task_idx: usize) -> Result<TASK, Error> {
+			let task = self
+				.lock()
+				.get(job_id)
+				.and_then(|(_, tasks)| tasks.get(task_idx))
+				.cloned();
+			task.ok_or_else(|| Error::new(ErrorKind::NotFound, "Task not found"))
+		}
 	}
 
 	#[cfg(test)]
 	mod test {
+		use std::io::ErrorKind;
+
 		use uuid::Uuid;
 
 		use crate::job_db::local::LocalJobDb;
@@ -82,6 +109,49 @@ mod local {
 			let id = manager.create_job(job.clone()).await.unwrap();
 			let res = manager.get_job(&id).await.unwrap().unwrap();
 			assert_eq!(res, job)
+		}
+
+		#[tokio::test]
+		async fn add_task_to_nonexistent_job_error() {
+			let manager = LocalJobDb::<String, String>::default();
+			let task = "Task 1".to_string();
+			let first_task = manager.append_task(&Uuid::from_u64_pair(1, 2), task).await;
+			assert_eq!(first_task.unwrap_err().kind(), ErrorKind::NotFound)
+		}
+
+		#[tokio::test]
+		async fn get_task_nonexistent_job_error() {
+			let manager = LocalJobDb::<String, String>::default();
+			let res = manager.get_task(&Uuid::from_u64_pair(1, 2), 0).await;
+			assert_eq!(res.unwrap_err().kind(), ErrorKind::NotFound)
+		}
+
+		#[tokio::test]
+		async fn add_get_task_info_by_id() {
+			let manager = LocalJobDb::<String, String>::default();
+			let task = "Task 1".to_string();
+			let job = "Job 1".to_string();
+			let job_id = manager.create_job(job.clone()).await.unwrap();
+			let task_idx = manager.append_task(&job_id, task.clone()).await.unwrap();
+			let res = manager.get_task(&job_id, task_idx).await.unwrap();
+			assert_eq!(task, res)
+		}
+
+		#[tokio::test]
+		async fn add_task_to_job_returns_sequencial_id() {
+			let manager = LocalJobDb::<String, String>::default();
+			let job = "Job 1".to_string();
+			let task_1 = "Task 1".to_string();
+			let task_2 = "Task 1".to_string();
+			let id = manager.create_job(job.clone()).await.unwrap();
+			let first_task = manager.append_task(&id, task_1).await.unwrap();
+			let second_task = manager.append_task(&id, task_2).await.unwrap();
+			assert!(
+				second_task > first_task,
+				"Second task should have a greater id than first task, {} > {}",
+				second_task,
+				first_task
+			);
 		}
 	}
 }
