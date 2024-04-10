cd7cc934760acf150086394c83f439949167863c
Eduardo M Gomes <me@emg.dev>
2024-04-10 19:42:58 -0300

    Make manager async

--- application/task/src/task.rs
+++ application/task/src/task.rs
@@ -39,17 +39,18 @@ struct Instance {
 	recipe: Recipe,
 }
 
+///Interface used by the server to manage jobs and tasks
 trait Manager {
-	fn crate_job(job: JobSource) -> Uuid;
-	fn allocate_task() -> Instance;
-	fn add_task_to_job(job_id: &Uuid, task: TaskSource) -> u32;
-	fn get_task(job_id: &Uuid, task_id: &Uuid) -> Instance;
-	fn update_task_status(job_id: &Uuid, task_id: &Uuid);
-	fn set_task_output(job_id: &Uuid, task_id: &Uuid);
+	async fn crate_job(job: JobSource) -> Result<Uuid, std::io::Error>;
+	async fn allocate_task() -> Result<Option<Instance>, std::io::Error>;
+	async fn add_task_to_job(job_id: &Uuid, task: TaskSource) -> Result<u32, std::io::Error>;
+	async fn get_task(job_id: &Uuid, task_id: &Uuid) -> Result<Instance, std::io::Error>;
+	async fn update_task_status(job_id: &Uuid, task_id: &Uuid) -> Result<(), std::io::Error>;
+	async fn set_task_output(job_id: &Uuid, task_id: &Uuid) -> Result<(), std::io::Error>;
 	///Cancel this task execution, will be available for allocation
-	fn cancel_task(job_id: &Uuid, task_id: &Uuid);
+	async fn cancel_task(job_id: &Uuid, task_id: &Uuid) -> Result<(), std::io::Error>;
 	///Delete the job removing all tasks, completed or pending
-	fn delete_job(job_id: &Uuid);
+	async fn delete_job(job_id: &Uuid) -> Result<(), std::io::Error>;
 }
 
 mod manager;
