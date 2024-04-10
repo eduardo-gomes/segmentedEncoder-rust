313137061076c0ba319d632a464e0944d22233b1
Eduardo M Gomes <me@emg.dev>
2024-04-10 19:26:32 -0300

    Add types to support api on task crate

--- application/task/src/task.rs
+++ application/task/src/task.rs
@@ -3,24 +3,46 @@
 
 use uuid::Uuid;
 
-struct JobSource {}
+struct JobSource {
+	input_id: Uuid,
+	video_options: Options,
+}
 
-enum TaskSource {
+struct TaskSource {
+	inputs: Vec<Input>,
+	recipe: Recipe,
+}
+
+struct Options {
+	codec: String,
+	params: Vec<String>,
+}
+
+enum Recipe {
 	Analysis(),
-	Transcode(),
+	Transcode(Options),
+	Merge(Vec<u32>),
+}
+
+struct Input {
+	index: u32,
+	start: Option<f64>,
+	end: Option<f64>,
 }
 
 ///An allocated task
-struct TaskInstance {
+struct Instance {
 	job_id: Uuid,
 	task_id: Uuid,
+	inputs: Vec<Input>,
+	recipe: Recipe,
 }
 
 trait Manager {
 	fn crate_job(job: JobSource) -> Uuid;
-	fn allocate_task() -> TaskInstance;
+	fn allocate_task() -> Instance;
 	fn add_task_to_job(job_id: &Uuid, task: TaskSource) -> u32;
-	fn get_task(job_id: &Uuid, task_id: &Uuid) -> TaskInstance;
+	fn get_task(job_id: &Uuid, task_id: &Uuid) -> Instance;
 	fn update_task_status(job_id: &Uuid, task_id: &Uuid);
 	fn set_task_output(job_id: &Uuid, task_id: &Uuid);
 	///Cancel this task execution, will be available for allocation
