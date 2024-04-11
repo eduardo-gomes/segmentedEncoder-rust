d473d7c717ccba53526c59566d2ff76a85b0dd32
Eduardo M Gomes <me@emg.dev>
2024-04-11 20:46:49 -0300

    Create /login route

--- application/server/src/api.rs
+++ application/server/src/api.rs
@@ -1,14 +1,30 @@
 //! Api based on api.yaml spec
 
+use axum::http::{HeaderMap, HeaderName, StatusCode};
 use axum::routing::get;
 use axum::Router;
 
 pub fn make_router() -> Router {
-	Router::new().route("/version", get(|| async { env!("CARGO_PKG_VERSION") }))
+	Router::new()
+		.route("/version", get(|| async { env!("CARGO_PKG_VERSION") }))
+		.route("/login", get(login))
+}
+
+async fn login(header_map: HeaderMap) -> StatusCode {
+	let credentials = header_map
+		.get(HeaderName::from_static("credentials"))
+		.map(|v| v.to_str())
+		.transpose()
+		.unwrap_or_default();
+	match credentials {
+		None => StatusCode::BAD_REQUEST,
+		Some(_) => StatusCode::FORBIDDEN,
+	}
 }
 
 #[cfg(test)]
 mod test {
+	use axum::http::{HeaderName, HeaderValue, StatusCode};
 	use axum_test::TestServer;
 
 	use crate::api::make_router;
@@ -33,5 +49,26 @@ mod test {
 			version.contains(expected),
 			"Got {version}, expected {expected}"
 		);
+	}
+
+	#[tokio::test]
+	async fn get_login_without_auth_bad_request() {
+		let server = test_server();
+		let status = server.get("/login").await.status_code();
+		assert_eq!(status, StatusCode::BAD_REQUEST);
+	}
+
+	#[tokio::test]
+	async fn get_login_with_bad_auth_forbidden() {
+		let server = test_server();
+		let status = server
+			.get("/login")
+			.add_header(
+				HeaderName::from_static("credentials"),
+				HeaderValue::from_static("bad auth"),
+			)
+			.await
+			.status_code();
+		assert_eq!(status, StatusCode::FORBIDDEN);
 	}
 }
