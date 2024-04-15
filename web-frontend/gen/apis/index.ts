f4fc598c9978f33716da51d700ea52b52c95c602
Eduardo M Gomes <me@emg.dev>
2024-04-15 00:01:48 -0300

    Commit generated typescript code to avoid using code generator on pipeline

---
+++ web-frontend/gen/apis/index.ts
@@ -0,0 +1,5 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DefaultApi';
+export * from './NiceApi';
+export * from './WorkerApi';
---
+++ web-frontend/gen/models/index.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AnalysisTask';
+export * from './CodecParams';
+export * from './Recipe';
+export * from './Task';
+export * from './TaskInputInner';
+export * from './TranscodeTask';
