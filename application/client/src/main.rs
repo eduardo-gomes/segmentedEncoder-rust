657f7a97996965b9849eb6bc82ae584d57b3b74c
Eduardo M Gomes <me@emg.dev>
2024-04-18 19:54:36 -0300

    Make client receive and parse server url

--- application/client/src/main.rs
+++ application/client/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command()]
+struct Args {
+	///Server api base url
+	#[arg(short, long, default_value = "http://localhost:8888/api")]
+	server: String,
+}
+
 fn main() {
+	let args = Args::parse();
+	let base = args
+		.server
+		.parse::<reqwest::Url>()
+		.expect("Should be valid uri");
+	println!("Server: {}", base);
 	unimplemented!()
 }
