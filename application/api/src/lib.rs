5b2f1093113c695291d1b41908adccc99090b11a
Eduardo M Gomes <me@emg.dev>
2024-04-18 23:39:29 -0300

    Add client feature to api

--- application/api/src/lib.rs
+++ application/api/src/lib.rs
@@ -4,3 +4,8 @@ extern crate serde_derive;
 pub mod models {
 	include!(concat!(env!("OUT_DIR"), "/generated/src/models/mod.rs"));
 }
+
+#[cfg(feature = "client")]
+pub mod apis {
+	include!(concat!(env!("OUT_DIR"), "/generated/src/apis/mod.rs"));
+}
