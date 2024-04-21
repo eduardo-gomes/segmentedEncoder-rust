54c32d6f8c05984c78111e28c8e4b7a0785cfb30
Eduardo M Gomes <me@emg.dev>
2024-04-21 09:35:27 -0300

    Allow unused imports on generated code

--- application/api/src/lib.rs
+++ application/api/src/lib.rs
@@ -1,11 +1,13 @@
 #[macro_use]
 extern crate serde_derive;
 
+#[allow(unused_imports)]
 pub mod models {
 	include!(concat!(env!("OUT_DIR"), "/generated/src/models/mod.rs"));
 }
 
 #[cfg(feature = "client")]
+#[allow(unused_imports)]
 pub mod apis {
 	include!(concat!(env!("OUT_DIR"), "/generated/src/apis/mod.rs"));
 }
