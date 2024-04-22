d82ca41c629e98ffe1a73452421160aaafde41c6
Eduardo M Gomes <me@emg.dev>
2024-04-22 18:10:26 -0300

    Make client run dummy task

--- application/client/src/lib.rs
+++ application/client/src/lib.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 use uuid::Uuid;
 
 use api::apis::configuration::Configuration;
-use task::Instance;
+use task::{Instance, Status};
 
 #[allow(async_fn_in_trait)]
 pub trait TaskRunner {
@@ -13,6 +13,7 @@ pub trait TaskRunner {
 	fn get_output_creds(&self) -> String {
 		self.get_input_creds()
 	}
+	async fn mark_task_complete(&self, job: Uuid, task: Uuid) -> Result<(), ()>;
 
 	async fn run(&self, task: Instance) {
 		let inputs = task
@@ -38,6 +39,7 @@ pub trait TaskRunner {
 				args
 			})
 			.collect::<Vec<_>>();
+		let params = ["-c", "copy"].into_iter().map(String::from);
 		let output = [
 			"-f".to_string(),
 			"matroska".to_string(),
@@ -48,8 +50,19 @@ pub trait TaskRunner {
 			self.get_output_url(task.job_id, task.task_id),
 		];
 		let mut ffmpeg = Command::new("ffmpeg");
-		ffmpeg.args(inputs.into_iter()).args(output.into_iter());
+		ffmpeg
+			.args(inputs.into_iter())
+			.args(params)
+			.args(output.into_iter());
 		println!("Command: {:?}", ffmpeg);
+		let status = ffmpeg
+			.status()
+			.expect("Failed to run ffmpeg")
+			.code()
+			.unwrap();
+		println!("ffmpeg returned: {status}");
+		let res = self.mark_task_complete(task.job_id, task.task_id).await;
+		println!("Mark task complete: {:?}", res);
 	}
 }
 
@@ -67,5 +80,16 @@ impl TaskRunner for Configuration {
 			.as_ref()
 			.map(|k| k.key.to_string())
 			.unwrap_or_default()
+	}
+
+	async fn mark_task_complete(&self, job: Uuid, task: Uuid) -> Result<(), ()> {
+		let res = api::apis::worker_api::job_job_id_task_task_id_status_post(
+			self,
+			&job.to_string(),
+			&task.to_string(),
+			Some(Status::Finished.into()),
+		)
+		.await;
+		res.or(Err(()))
 	}
 }
