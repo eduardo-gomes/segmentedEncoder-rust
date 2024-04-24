d3bf4fec635bccb4af3a5d30f6b977d1631cbc9b
Eduardo M Gomes <me@emg.dev>
2024-04-24 15:32:09 -0300

    Make client generate task with hard coded parameters

--- application/client/src/lib.rs
+++ application/client/src/lib.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 use uuid::Uuid;
 
 use api::apis::configuration::Configuration;
-use task::{Instance, Status};
+use task::{Input, Instance, Options, Recipe, Status, TaskSource};
 
 #[allow(async_fn_in_trait)]
 pub trait TaskRunner {
@@ -15,7 +15,22 @@ pub trait TaskRunner {
 	}
 	async fn mark_task_complete(&self, job: Uuid, task: Uuid) -> Result<(), ()>;
 
-	async fn run(&self, task: Instance) {
+	async fn add_task_to_job(&self, job: Uuid, task: TaskSource) -> Result<(), ()>;
+
+	async fn run_analysis(&self, task: Instance, option: Option<f64>) -> Result<(), ()> {
+		let source = TaskSource {
+			inputs: vec![Input::source()],
+			recipe: Recipe::Transcode(Options {
+				codec: "libx264".to_string(),
+				params: ["-preset", "ultrafast", "-crf", "30"]
+					.into_iter()
+					.map(String::from)
+					.collect(),
+			}),
+		};
+		self.add_task_to_job(task.job_id, source).await
+	}
+	async fn run_transcode(&self, task: Instance, recipe: Options) -> Result<(), ()> {
 		let inputs = task
 			.inputs
 			.into_iter()
@@ -39,7 +54,10 @@ pub trait TaskRunner {
 				args
 			})
 			.collect::<Vec<_>>();
-		let params = ["-c", "copy"].into_iter().map(String::from);
+		let codec = ["-c:v", recipe.codec.as_str()]
+			.into_iter()
+			.map(String::from);
+		let params = recipe.params.into_iter();
 		let output = [
 			"-f".to_string(),
 			"matroska".to_string(),
@@ -52,6 +70,7 @@ pub trait TaskRunner {
 		let mut ffmpeg = Command::new("ffmpeg");
 		ffmpeg
 			.args(inputs.into_iter())
+			.args(codec)
 			.args(params)
 			.args(output.into_iter());
 		println!("Command: {:?}", ffmpeg);
@@ -63,6 +82,15 @@ pub trait TaskRunner {
 		println!("ffmpeg returned: {status}");
 		let res = self.mark_task_complete(task.job_id, task.task_id).await;
 		println!("Mark task complete: {:?}", res);
+		Ok(())
+	}
+
+	async fn run(&self, task: Instance) {
+		let _ = match task.recipe.clone() {
+			Recipe::Analysis(analysis) => self.run_analysis(task, analysis).await,
+			Recipe::Transcode(transcode) => self.run_transcode(task, transcode).await,
+			Recipe::Merge(_) => unimplemented!("Merge task is not implemented"),
+		};
 	}
 }
 
@@ -91,5 +119,23 @@ impl TaskRunner for Configuration {
 		)
 		.await;
 		res.or(Err(()))
+	}
+
+	async fn add_task_to_job(&self, job: Uuid, task: TaskSource) -> Result<(), ()> {
+		let recipe = match task.recipe {
+			Recipe::Transcode(t) => Some(t),
+			_ => None,
+		}
+		.ok_or(())?;
+		let parsed = api::models::TaskRequest {
+			inputs: vec![Input::source().into()],
+			recipe: Box::new(api::models::TaskRequestRecipe::TranscodeTask(Box::new(
+				recipe.into(),
+			))),
+		};
+		api::apis::worker_api::job_job_id_task_post(self, &job.to_string(), Some(parsed))
+			.await
+			.or(Err(()))
+			.and(Ok(()))
 	}
 }
