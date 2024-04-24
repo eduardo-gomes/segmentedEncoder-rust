655d1ae67c6eb59e3c1208ee4e6e683ecf5f31d3
Eduardo M Gomes <me@emg.dev>
2024-04-24 17:12:03 -0300

    Update generated frontend code

--- web-frontend/gen/models/index.ts
+++ web-frontend/gen/models/index.ts
@@ -2,7 +2,12 @@
 /* eslint-disable */
 export * from './AnalysisTask';
 export * from './CodecParams';
+export * from './JobOptions';
+export * from './MergeTask';
 export * from './Recipe';
 export * from './Task';
 export * from './TaskInputInner';
+export * from './TaskRequest';
+export * from './TaskRequestRecipe';
+export * from './TaskStatus';
 export * from './TranscodeTask';
