6f171429ef0155aa7e8dded9f406f00331ed9f63
Eduardo M Gomes <me@emg.dev>
2024-05-06 13:54:38 -0300

    Implement simple download output on frontend

--- web-frontend/src/components/tabs/status.tsx
+++ web-frontend/src/components/tabs/status.tsx
@@ -9,15 +9,40 @@ function statusWatcher({ id, api }: { id: string, api: Accessor<DefaultApi> }): 
 		trigger();
 		api().jobJobIdOutputGetRaw({ jobId: id }).then((response) => {
 			if (response.raw.ok) setStatus("finished");
-		}).finally(() => setTimeout(doTrigger, 5000));
+		}).catch(() => setTimeout(doTrigger, 5000));
 	})
 	return status
 }
 
 function Job(props: { id: string, api: Accessor<DefaultApi> }) {
 	const status = statusWatcher(props);
+	const ok = () => status() == "finished";
+	const [progress, setProgress] = createSignal<number | undefined>();
+
+	async function download() {
+		setProgress(0);
+		const response = await props.api().jobJobIdOutputGetRaw({ jobId: props.id }).then((r) => r.raw);
+		if (!response.body) throw new Error("No body to download");
+		const tracker = new TransformStream<Uint8Array, Uint8Array>({
+			transform(chunk, controller) {
+				setProgress((last) => (last ?? 0) + chunk.length);
+				controller.enqueue(chunk);
+			}
+		});
+		const blob = await (new Response(response.body.pipeThrough(tracker), { headers: { content_type: "video/matroska" } })).blob();
+		const url = URL.createObjectURL(blob);
+		const a = document.createElement<"a">('a')
+		a.href = url
+		a.download = props.id + ".mkv"
+		document.body.appendChild(a)
+		a.click()
+		document.body.removeChild(a)
+		URL.revokeObjectURL(url);
+	}
+
 	return (<div>
-		Job id: <samp>{props.id}</samp> output status: {status()}
+		Job id: <samp>{props.id}</samp> output status: {status()} {ok() ?
+		<button onClick={download}>Download</button> : null} {progress()}
 	</div>);
 }
 
@@ -38,7 +63,7 @@ function StatusTab(props: { visible: boolean }) {
 	});
 
 	return (<>
-		Auto refreshing /latest:
+		Auto refreshing job list:
 		<Switch fallback={<div style={{ "font-size": "x-large" }}>Not connected to the server</div>}>
 			<Match when={authenticated()}>
 				<For each={status()}>
