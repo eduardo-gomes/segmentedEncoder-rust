36d3cd6879d6887417a2758a3e943f3f2a1a231a
Eduardo M Gomes <me@emg.dev>
2024-05-06 01:50:46 -0300

    Implement job status on frontend for new api

--- web-frontend/src/components/tabs/status.tsx
+++ web-frontend/src/components/tabs/status.tsx
@@ -1,17 +1,49 @@
-import { createSignal, Match, Switch, useContext } from "solid-js";
+import { type Accessor, createEffect, createSignal, For, Match, onCleanup, Switch, useContext } from "solid-js";
 import { ApiContext } from "../../lib/apiProvider";
+import type { DefaultApi } from "@api";
+
+function statusWatcher({ id, api }: { id: string, api: Accessor<DefaultApi> }): Accessor<string> {
+	const [status, setStatus] = createSignal("pending");
+	const [trigger, doTrigger] = createSignal(undefined, { equals: false });
+	createEffect(() => {
+		trigger();
+		api().jobJobIdOutputGetRaw({ jobId: id }).then((response) => {
+			if (response.raw.ok) setStatus("finished");
+		}).finally(() => setTimeout(doTrigger, 5000));
+	})
+	return status
+}
+
+function Job(props: { id: string, api: Accessor<DefaultApi> }) {
+	const status = statusWatcher(props);
+	return (<div>
+		Job id: <samp>{props.id}</samp> output status: {status()}
+	</div>);
+}
 
 function StatusTab(props: { visible: boolean }) {
-	const [status, _setStatus] = createSignal("Not yet supported on new api");
+	const [status, setStatus] = createSignal<Array<string>>([]);
 
-	const {version} = useContext(ApiContext);
-	const is_connected = () => Boolean(version);
+	const { authenticated, api } = useContext(ApiContext);
+
+	const [trigger, doTrigger] = createSignal(undefined, { equals: false });
+	createEffect(() => {
+		trigger();
+		if (!props.visible) return;
+		const controller = new AbortController();
+		onCleanup(() => {
+			controller.abort("Cleanup")
+		});
+		api().jobGet({ signal: controller.signal }).then(setStatus).finally(() => setTimeout(doTrigger, 5000));
+	});
 
 	return (<>
 		Auto refreshing /latest:
-		<Switch fallback={<div style={{"font-size": "x-large"}}>Not connected to the server</div>}>
-			<Match when={is_connected()}>
-				<pre>{status()}</pre>
+		<Switch fallback={<div style={{ "font-size": "x-large" }}>Not connected to the server</div>}>
+			<Match when={authenticated()}>
+				<For each={status()}>
+					{(element) => <Job id={element} api={api}/>}
+				</For>
 			</Match>
 		</Switch>
 	</>);
